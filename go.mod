365808d2f76d1041c0a5cf6488c2b495d7ce8771
Artem Shahalov <tem.shahalov@gmail.com>
2024-12-15 14:46:30 +0000

    Using 2.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/artshah-coder/mod
+module github.com/artshah-coder/mod/v2
 
 go 1.23.3
--- mod.go
+++ mod.go
@@ -3,3 +3,10 @@ package mod
 func Swap(a, b int) (int, int) {
 	return b, a
 }
+
+func Sort2Int(a, b int) (int, int) {
+	if a > b {
+		return b, a
+	}
+	return a, b
+}
